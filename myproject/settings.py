e2cfc5de9a3ef793b850f2a7267fa8c5fa623553
Lauri Võsandi <lauri.vosandi@gmail.com>
2025-12-09 06:02:39 +0200

    Another test

--- myproject/settings.py
+++ myproject/settings.py
@@ -241,7 +241,9 @@ STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
         "OPTIONS": {
-            "bucket_name": "memelord",
+            "bucket_name": os.eviron.get("AWS_STORAGE_BUCKET_NAME"),
+            "endpoint_url": os.eviron.get("AWS_S3_ENDPOINT_URL"),
+            "region_name": os.environ.get("AWS_S3_REGION_NAME"),
         },
     },
     "staticfiles": {
